b944f4fa4ef9c0ea513d51452ab3050e6383e8ed
Nicola Lagnena <nlagnena@yahoo.it>
2020-01-13 22:52:58 +0100

    Updates

---
+++ precache-manifest.4daf957bb851fbeab4873c7c5598db4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5c1b9735bcb0ac75bbe61550634190",
+    "url": "/gfl-logistics/index.html"
+  },
+  {
+    "revision": "7d7debf6e008c01bc172",
+    "url": "/gfl-logistics/static/js/2.022c25bd.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/gfl-logistics/static/js/2.022c25bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e621b807bf6a354357b0",
+    "url": "/gfl-logistics/static/js/main.0cedf6c0.chunk.js"
+  },
+  {
+    "revision": "f5811e1b107fdd8c7394",
+    "url": "/gfl-logistics/static/js/runtime-main.aabfdb9a.js"
+  }
+]);
